f1510e7fb5f977901b62cfd5f837ca6c13d2960c
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-16 17:12:32 +0000

    b343af8c59e921a068a47d3ce933db0768114fee
    Sync to source repo @343af8c59e921a068a47d3ce933db0768114fee

--- js/scroller.bootstrap.mjs
+++ js/scroller.bootstrap.mjs
@@ -1,11 +1,10 @@
-
 /*! Bootstrap 3 styling wrapper for Scroller
  * © SpryMedia Ltd - datatables.net/license
  */
 
 import $ from 'jquery';
 import DataTable from 'datatables.net-bs';
-import DataTable from 'datatables.net-scroller';
+import Scroller from 'datatables.net-scroller';
 
 
 
